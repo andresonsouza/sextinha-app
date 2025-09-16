c41eded4c911daf28390bb666185681213f60448
Andreson Souza <andresonjazz@gmail.com>
2025-09-16 09:31:25 -0300

    chore: configure a primeira rota

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Sextinha App 🍲</h1>
+    <p>Bem-vindo ao nosso app de pedidos!</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Por enquanto nada aqui
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+  margin-top: 2rem;
+}
+</style>
